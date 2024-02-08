355a4f33bf61f8f74f618429ac1522d604747ef1
petar554 <petarburic554@gmail.com>
2024-02-08 19:44:14 +0100

    CRUD delete operation

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -2,8 +2,23 @@ const express = require('express');
 const router = express.Router();
 const gameController = require('../controllers/gameController');
 
+const gameService = require('../services/gameService');
+
 router.get('/', gameController.getAllGames);
 router.get('/search', gameController.searchGames);
 router.get('/pagination', gameController.getGamesWithPagination);
+
+// CRUD - delete
+router.delete('/:id', async (req, res) => {
+    try {
+        await gameService.deleteGame(req.params.id);
+        res.status(204).send(); 
+
+        console.error('The game has been deleted');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error deleting the game');
+    }
+});
 
 module.exports = router;
--- backend/routes/playerRoutes.js
+++ backend/routes/playerRoutes.js
@@ -2,10 +2,25 @@ const express = require('express');
 const router = express.Router();
 const playerController = require('../controllers/playerController');
 
+const playerService = require('../services/playerService');
+
 // routes
 router.get('/', playerController.getAllPlayers);
 router.get('/search', playerController.searchPlayers);
 router.get('/pagination', playerController.getPlayersWithPagination);
 router.get('/:playerId/games', playerController.getGamesPlayedByPlayer);
+
+// CRUD - delete
+router.delete('/:id', async (req, res) => {
+    try {
+        await playerService.deletePlayer(req.params.id);
+        res.status(204).send();
+
+        console.error('The player has been deleted');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error deleting the player');
+    }
+});
 
 module.exports = router;
