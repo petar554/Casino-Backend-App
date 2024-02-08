4496a6a07bf576b3bffd4830b4d8884decd3a6b9
petar554 <petarburic554@gmail.com>
2024-02-08 20:18:05 +0100

    CRUD update operation

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -13,11 +13,23 @@ router.get('/pagination', gameController.getGamesWithPagination);
 router.delete('/:id', async (req, res) => {
     try {
         await gameService.deleteGame(req.params.id);
-        console.log(`The game with ID ${req.params.id} has been deleted.`);
+        console.log(`The game with ID: ${req.params.id} has been deleted.`);
         res.status(204).send(); 
     } catch (error) {
         console.error(error);
         res.status(500).send('Error deleting the game');
+    }
+});
+
+// Test: curl -X PUT -H "Content-Type: application/json" -d '{"title":"GTA2","description":"Description of the GTA2"}' http://localhost:3000/api/games/{8ac7cd09-35d0-4959-9a3f-dfabef5677c4}
+router.put('/:gameId', async (req, res) => {
+    try {
+        const updatedGame = await gameService.updateGame(req.params.gameId, req.body);
+        console.log(`The game with ID: ${req.params.gameId} has been updated.`);
+        res.json(updatedGame);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error updating the game');
     }
 });
 
--- backend/routes/playerRoutes.js
+++ backend/routes/playerRoutes.js
@@ -14,11 +14,23 @@ router.get('/:playerId/games', playerController.getGamesPlayedByPlayer);
 router.delete('/:id', async (req, res) => {
     try {
         await playerService.deletePlayer(req.params.id);
-        console.log(`The player with ID ${req.params.id} has been deleted.`);
+        console.log(`The player with ID: ${req.params.id} has been deleted.`);
         res.status(204).send();
     } catch (error) {
         console.error(error);
         res.status(500).send('Error deleting the player');
+    }
+});
+
+// Test: curl -X PUT -H "Content-Type: application/json" -d '{"firstName":"Jan","lastName":"Bricl"}' http://localhost:3000/api/players/{34ba8d53-2fd0-47e1-bb52-ab02e2193483}
+router.put('/:playerId', async (req, res) => {
+    try {
+        const updatedPlayer = await playerService.updatePlayer(req.params.playerId, req.body);
+        console.log(`The plaer with ID: ${req.params.playerId} has been updated.`);
+        res.json(updatedPlayer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error updating the player');
     }
 });
 
